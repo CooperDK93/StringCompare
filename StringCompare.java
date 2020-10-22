34fcfd7eb6500f509bd523eb6b7ca5ed3166a9c9
CooperDK93 <68040090+CooperDK93@users.noreply.github.com>
2020-10-22 09:02:18 -0500

    Add files via upload

---
+++ StringCompare.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StringCompare {
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter the first string: ");
+            String inputOne = scan.nextLine();
+            System.out.println("Enter the second string: ");
+            String inputTwo = scan.nextLine();
+
+            if(inputOne.equals(inputTwo)){
+                System.out.println("Same");
+            }else{
+                System.out.println("Different");
+            }
+
+
+
+        }
+    }
+
+
+
